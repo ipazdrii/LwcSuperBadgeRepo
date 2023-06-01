36eac3a6f354a70344913bd55225ca3e814ccabb
ipazdrii <ihor.pazdrii@redtag.pro>
2023-06-01 14:45:54 +0100

    Build the Search Form and Add It to a New Lightning Page

---
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,25 @@
+// imports
+
+export default class BoatSearch extends LightningElement {
+    isLoading = false;
+
+    // Handles loading event
+    handleLoading(){
+
+    }
+
+    // Handles done loading event
+    handleDoneLoading(){
+
+    }
+
+    // Handles search boat event
+    // This custom comes from the form
+    searchBoats(event){
+
+    }
+
+    createNewBoat(){
+        
+    }
+}
